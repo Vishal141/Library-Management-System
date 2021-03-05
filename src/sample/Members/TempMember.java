4672fb41952623d10752ba785062d342ef5447bc
vishal <vishalahirwar9669@gmail.com>
2021-03-05 22:14:25 +0530

    Spring Boot server added in books features

--- src/sample/Members/TempMember.java
+++ src/sample/Members/TempMember.java
@@ -1,4 +1,47 @@
 package sample.Members;
 
 public class TempMember {
+    private String name;
+    private String id;
+    private String emailId;
+    private String mobileNo;
+
+    public TempMember(String name, String id, String emailId, String mobileNo) {
+        this.name = name;
+        this.id = id;
+        this.emailId = emailId;
+        this.mobileNo = mobileNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
 }
